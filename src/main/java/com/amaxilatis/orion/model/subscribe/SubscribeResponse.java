4aeef12a7b2fcda3907dd7e46b820552907248f4
Evangelos Theodoridis <evangelos.theodoridis@intel.com>
2015-11-13 12:36:15 +0000

    subscribeError on response

--- src/main/java/com/amaxilatis/orion/model/subscribe/SubscribeResponse.java
+++ src/main/java/com/amaxilatis/orion/model/subscribe/SubscribeResponse.java
@@ -1,13 +1,20 @@
 package com.amaxilatis.orion.model.subscribe;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 /**
  * @author Dimitrios Amaxilatis.
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SubscribeResponse {
 
     String subscriptionId;
     String duration;
     String throttling;
+    String subscribeError;
+
+    public SubscribeResponse() {
+    }
 
     public String getSubscriptionId() {
         return subscriptionId;
@@ -31,6 +38,14 @@ public class SubscribeResponse {
 
     public void setThrottling(String throttling) {
         this.throttling = throttling;
+    }
+
+    public String getSubscribeError() {
+        return subscribeError;
+    }
+
+    public void setSubscribeError(String subscribeError) {
+        this.subscribeError = subscribeError;
     }
 
     @Override
